963228246e96d25d15f0fcc2c3365b49b829ce2d
Jade Vargas <jadevargas92@gmail.com>
2017-01-26 21:11:12 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pajamawork::Application.config.secret_key_base = '1f5b602a6920ac00150bebc1026f753f62324a6d24b27f43aca98c64c8e8d7cb2648f7af7c34c18aa29484941a321ed5b87b355c986b47d6c7e11f58ba5d8216'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pajamawork::Application.config.session_store :cookie_store, key: '_pajamawork_session'
