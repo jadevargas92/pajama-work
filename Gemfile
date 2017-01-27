cf35d44343b240735cf66f4f4c1bde65f9c0970b
Jade Vargas <jadevargas92@gmail.com>
2017-01-27 04:14:14 +0000

    Added database and Modal

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'bootstrap-sass', '~> 3.3.5.1'
 
+gem 'bootstrap_form'
+
 gem 'sprockets', '2.11.0'
 
 gem 'rails_12factor', group: :production
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,20 @@
 class JobsController < ApplicationController
 	def index
-		@jobs = Job.all
+		@job = Job.all
 	end
 
 	def new
+		@job = Job.new
+	end
+
+	def create
+		Job.create(job_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def job_params
+		params.require(:job).permit(:title, :description, :company)
 	end
 end
